3f67c23a1d1af5b36e62d3992bd454a285643d03
Ethan Vector <valvod486@gmail.com>
2026-02-05 12:56:07 +0100

    Delete test_embeddings.py

--- test_embeddings.py
+++
@@ -1,9 +0,0 @@
-from rag_observatory.embeddings import Embedder
-
-
-def test_embedder_deterministic():
-    e = Embedder(dim=64)
-    v1 = e.embed("hello world")
-    v2 = e.embed("hello world")
-    assert v1 == v2
-    assert len(v1) == 64
